fe64485ba59f90db198b1d79a2edcf7d03d7b74f
greenTeaFrappe <105686638@student.swin.edu.au>
2025-05-18 21:46:58 +1000

    Add: added sql file

---
+++ project1/pixelprotectors_db.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2025 at 01:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pixelprotectors_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eoi`
+--
+
+CREATE TABLE `eoi` (
+  `EOInumber` int(11) NOT NULL,
+  `JobReferenceNumber` varchar(50) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `StreetAddress` varchar(100) NOT NULL,
+  `Suburb` varchar(50) NOT NULL,
+  `State` varchar(20) NOT NULL,
+  `Postcode` varchar(10) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `PhoneNumber` varchar(20) NOT NULL,
+  `Python` varchar(50) DEFAULT NULL,
+  `Adobe_Illustrator` varchar(50) DEFAULT NULL,
+  `JAVA` varchar(50) DEFAULT NULL,
+  `MySQL` varchar(50) DEFAULT NULL,
+  `OtherSkills` text DEFAULT NULL,
+  `Status` enum('New','Current','Final') NOT NULL DEFAULT 'New'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eoi`
+--
+ALTER TABLE `eoi`
+  ADD PRIMARY KEY (`EOInumber`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eoi`
+--
+ALTER TABLE `eoi`
+  MODIFY `EOInumber` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- project1/process_eoi.php
+++ project1/process_eoi.php
@@ -1,4 +1,29 @@
 <?php
-session_start();
+require_once "settings.php";
+$dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
+if($dbconn){
+    $query = "SELECT * FROM EOI";
 
+    $createTableQuery = "CREATE TABLE IF NOT EXISTS eoi (
+        EOInumber INT AUTO_INCREMENT PRIMARY KEY,
+        JobReferenceNumber VARCHAR(50) NOT NULL,
+        FirstName VARCHAR(20) NOT NULL,
+        LastName VARCHAR(20) NOT NULL,
+        StreetAddress VARCHAR(40) NOT NULL,
+        Suburb VARCHAR(40) NOT NULL,
+        State VARCHAR(3) NOT NULL,
+        Postcode VARCHAR(4) NOT NULL,
+        Email VARCHAR(100) NOT NULL,
+        PhoneNumber VARCHAR(20) NOT NULL,
+        Python VARCHAR(50),
+        Adobe_Illustrator VARCHAR(50),
+        JAVA VARCHAR(50),
+        MySQL VARCHAR(50),
+        OtherSkills TEXT,
+        Status ENUM('New', 'Current', 'Final') DEFAULT 'New' NOT NULL
+    )";
+
+    $result = mysqli_query($dbconn, $query);
+   
+}
 ?>
