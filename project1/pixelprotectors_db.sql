6990e1f51b64fb80a249d6a4ce137a3b3a5f07d8
Vanessa Giuliano <108853801@student.swin.edu.au>
2025-05-19 11:58:23 +1000

    Added jobs table into database

--- project1/pixelprotectors_db.sql
+++ project1/pixelprotectors_db.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 18, 2025 at 01:45 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: May 19, 2025 at 03:55 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,6 +46,31 @@ CREATE TABLE `eoi` (
   `Status` enum('New','Current','Final') NOT NULL DEFAULT 'New'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `responsibilites` text NOT NULL,
+  `requirements` text NOT NULL,
+  `salary` varchar(25) NOT NULL,
+  `ref_number` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `title`, `description`, `responsibilites`, `requirements`, `salary`, `ref_number`) VALUES
+(1, 'Cybersecurity Specialist', 'Protect computers and data from hackers. As a cybersecurity specialist you will help maintain secure infrastructures and manage cyber attacks. You will work under and report to our Security Director.', 'Maintain infrastructures for secure data storage\r\nRespond to cyber threats\r\nImplement malware software\r\nUse appropriate cyber security tools\r\nCollaborate with the security team to create and analyse evasion tactics', 'Teamwork skills\r\nMinimum 3 years experience in a similar role\r\nMinimum academic requirement - Bachelors Degree in Cybersecurity or adjacent;\r\nExperience with programming languages including:\r\n- JavaScript\r\n- Python\r\n- SQL\r\nKnowledge of Linux operating system', '90,000 - 110,000', 'CSS01'),
+(2, 'Data Analyst', 'Working with us as a Data Analyst you will process and interpret data to help us make informed decisions. You will work in the Data Analysis team, under the guidance of a Senior Analyst.', 'Maintain exisiting infrastructures\r\nCollect, clean, analyse and interpret data\r\nAnalyse data to identify trends\r\nHelp improve reliability and efficiency of analysis models\r\nProvide reports on performance and analytics', 'Attention to detail\r\nMinimum 2 years experience in a similar role\r\nMinimum academic requirement - Bachelors Degree in Data Science or adjacent\r\nExperience with the following languages:\r\n- SQL\r\n- R\r\nExperience with Tableau\r\nKnowledge of Linux operating system', '65,000 - 80,000', 'DA002'),
+(3, 'Software Engineer', 'Update old and program new software to improve business performance. You will work on projects with a programming team, and report to a Project Manager.', 'Maintain and debug software programs\r\nWork on projects in cross-departmental teams\r\nImprove software performance and optimise programs\r\nApply testing and quality assurance\r\nAdhere to programming standards', 'Good teamwork skills\r\nMinimum 2 years experience in a similar role\r\nMinimum academic requirement - Bachelors Degree in Computer Science or adjacent\r\nExperience with the following languages:\r\n- JavaScript\r\n- Python\r\n- C++\r\nGreat problem solving skills', '82,000 - 110,000', 'SE003');
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +82,12 @@ ALTER TABLE `eoi`
   ADD PRIMARY KEY (`EOInumber`);
 
 --
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +96,12 @@ ALTER TABLE `eoi`
 --
 ALTER TABLE `eoi`
   MODIFY `EOInumber` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
